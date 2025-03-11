7df85addd78033f0c5c358d29d90abb105142b2a
alIthestars <leticiacquinteiro@gmail.com>
2025-03-11 10:12:40 -0300

    Feat: Trabalhando com variaveis

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+var idade = 20;
+let nome = "Leticia";
+let ativo = true;
+const PI = 3.14;
+
+//string para numero
+let num = Number("81")
+let flost = parseFloat("6.6")
+let inteiro = parseInt("10.5")
+
+console.log(idade)
+console.log(nome)
+console.log(ativo)
+console.log(PI)
+
+if(true){
+    console.log(idade)
+    console.log(nome)
+    console.log(ativo)
+    console.log(PI)
+}
