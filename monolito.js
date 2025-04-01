6823c6f6a1fa1b5df32824435844a17a7651b6c6
alIthestars <leticiacquinteiro@gmail.com>
2025-04-01 11:30:39 -0300

    Atualização

--- monolito.js
+++ monolito.js
@@ -18,6 +18,20 @@ app.get('/user_data', (req, res) => {
     res.send({users});
 });
 
+app.post('/pedido', (req, res) => {
+    const pedido  = req.body;
+    pedidos.push(pedido);
+    res.send({message: 'Pedido cadastrado com sucesso!', pedido});
+});
+
+app.get('/pedido_data', (req, res) => {
+    res.send({pedidos});
+});
+
+app.get('/all_data',(req, res) => {
+    res.send({users, pedidos});
+});
+
 app.listen(3000, () => {
     console.log('Servidor monolítico rodando na porta 3000');
 });
